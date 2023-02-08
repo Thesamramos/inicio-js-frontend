27353dcfd67796245b4458c3c8e1f121bcccf7a3
Samuel Ramos <104317445+Thesamramos@users.noreply.github.com>
2023-02-08 16:50:41 -0400

    coletar dados

---
+++ script.js
@@ -0,0 +1,8 @@
+let tratamentoPersonalizado = confirm("Deseja um tratamento personalizado?")
+
+if(tratamentoPersonalizado){
+    let nome = prompt("Por favor, digite seu nome")
+        alert("Bem-vindo ao site "+ nome +". Muito obrigado pela sua visita, estamos a sua disposição!")
+}else{
+    alert("Obrigado por conectar.")
+}
